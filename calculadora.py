b25e50dac9900b3fab6211b74e93cbc1ff84f47d
user.name <denysiuk.isi@gmail.com>
2021-01-26 19:54:45 -0300

    menu seleccion operaciones

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,14 @@
 a = int(input("ingrese un numero: "))
 b = int(input("ingrese otro numero: "))
 
-print(a+b)
+opcion = 0
+while opcion not in (1,2,3):
+    print("1-Suma")
+    print("2-Resta")
+
+    opcion = int(input("ingrese opcion: "))
+
+if opcion == 1:
+    print(a+b)
+else:
+    print(a-b)
