50c744fa10454ca48d3bc02529c72fa578623ca5
user.name <denysiuk.isi@gmail.com>
2021-01-26 20:09:24 -0300

    funcion resta

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,9 @@
 a = int(input("ingrese un numero: "))
 b = int(input("ingrese otro numero: "))
 
+def restar(num_a, num_b):
+    return num_a - num_b
+
 opcion = 0
 while opcion not in (1,2,3):
     print("1-Suma")
@@ -11,4 +14,4 @@ while opcion not in (1,2,3):
 if opcion == 1:
     print(a+b)
 else:
-    print(a-b)
+    print(restar(a,b))
